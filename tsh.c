2c04ca3476c7dbd2b6e82d8806f1413abc403f5a
Jón Reginbald <joni12@ru.is>
2015-02-11 20:52:51 +0000

    breytti exit

--- tsh.c
+++ tsh.c
@@ -243,7 +243,7 @@ int parseline(const char *cmdline, char **argv)
  */
 int builtin_cmd(char **argv) 
 {
-	if (strcmp(argv[0], "quit")){ 	/* quit command */
+	if (!strcmp(argv[0], "quit")){ 	/* quit command */
 		exit(0);
 	}
     return 0;     /* not a builtin command */
@@ -278,6 +278,11 @@ void waitfg(pid_t pid)
  */
 void sigchld_handler(int sig) 
 {
+	pid_t pid;
+	while ((pid = waitpid(-1, NULL, 0)) > 0) /* Reap a zombie child */
+		deletejob(pid); /* Delete the child from the job list */
+	if (errno != ECHILD)
+		unix_error("waitpid error");
     return;
 }
 
